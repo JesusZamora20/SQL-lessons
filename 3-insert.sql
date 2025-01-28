fa218dbc5a4b29f18532a99a89ec2574c5ff27e4
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-01-27 20:28:42 -0500

    class 3, insert values

---
+++ 3-insert.sql
@@ -0,0 +1,8 @@
+
+-- describe usuarios;
+
+insert into usuarios (nombre, clave) values ('Jesus','Bermudez');
+insert into usuarios (nombre, clave) values ('Yolanda','Lozano');
+
+-- ver registros de tabla
+select nombre, clave from usuarios;
