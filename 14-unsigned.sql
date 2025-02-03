7ce5206dd6f33af2d322aaf0e7cde130cfca9687
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-02-03 16:10:34 -0500

    class 14, unsigned

---
+++ 14-unsigned.sql
@@ -0,0 +1,15 @@
+-- unsigned only allows positive values
+
+drop table if exists libros;
+
+create table libros(
+  codigo integer unsigned auto_increment,
+  titulo varchar(20) not null,
+  autor varchar(30),
+  editorial varchar(15),
+  precio float unsigned,
+  cantidad integer unsigned,
+  primary key (codigo)
+ );
+
+describe libros;
